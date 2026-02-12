6912d3f0aa7db714270ad3bbef93bc3b40065fdf
Denis Sadovskii <sadovskidg@mail.ru>
2026-02-12 18:56:18 +0300

    Random string module added

--- internal/config/http-server/handlers/url/save/save.go
+++ internal/config/http-server/handlers/url/save/save.go
@@ -1,6 +1,9 @@
 package save
 
 import (
+	"fmt"
+	"modules/internal/config/lib/api/response"
+	"modules/internal/config/lib/logger/sl"
 	"net/http"
 
 	"golang.org/x/exp/slog"
@@ -12,9 +15,8 @@ type Request struct {
 }
 
 type Response struct {
-	Status string `json:"status"`
-	Error  string `json:"error,omitempty"`
-	Alias  string `json:"alias,omitempty"`
+	Response response.Response
+	Alias    string `json:"alias,omitempty"`
 }
 
 type URLSaver interface {
@@ -24,5 +26,32 @@ type URLSaver interface {
 func New(log *slog.Logger, saver URLSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Info("Save URL handler called")
+
+		var req Request
+		err := render.DecodeJSON(r.body, &req)
+		if err != nil {
+			fmt.Errorf("Failed to decode request body: %w", err)
+
+			render.JSON(w, r, response.Error("Invalid request body"))
+
+			return
+		}
+
+		log.Info("Request body decoded", "url", req.URL, "alias", req.Alias)
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("Validation failed", sl.Err(err))
+
+			render.JSON(w, r, response.Error("Invalid request body"))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
 	}
 }
--- internal/config/lib/logger/sl/sl.go
+++ internal/config/lib/logger/sl/sl.go
@@ -3,7 +3,6 @@ package sl
 import (
 	"golang.org/x/exp/slog"
 
-	_ "github.com/mattn/go-sqlite3"
 )
 
 func Err(err error) slog.Attr {
